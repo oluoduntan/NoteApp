7c3bb387cc9944175c8f3dc9e6425cbaf10b2978
Oluwadamilola Oduntan <oduntandamilola@gmail.com>
2024-01-07 23:58:10 -0500

    Started adding authentication

--- Note/urls.py
+++ Note/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('notes/', views.NoteList.as_view(), name="notes"),
     path('notes/<int:pk>', views.NoteDetail.as_view(), name="note"),
     path('user/', views.UserDetail.as_view(), name='user'),
-    path('register/', views.RegisterUser.as_view(), name='user-create'),
+    path('register/', views.RegisterUser.as_view(), name='register'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
